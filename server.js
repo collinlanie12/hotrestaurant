66580674377595c561eac0eb45e4eb9e7f57f585
Collin Lanier <collinlanier12@gmail.com>
2020-09-02 14:15:09 -0400

    added routes

--- server.js
+++ server.js
@@ -16,9 +16,35 @@ app.use(express.json());
 
 // Routes
 // =============================================================
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
 
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
 
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
+app.get("/api/patrons", function (req, res) {
+    return res.json(patrons);
+});
+
+app.get("/api/patrons/:patrons", function (req, res) {
+    var chosen = req.params.patrons;
+
+    console.log(chosen);
+
+    for (var i = 0; i < patrons.length; i++) {
+        if (chosen === patrons[i].routeName) {
+            return res.json(patrons[i]);
+        }
+    }
+
+    return res.json(false);
+});
 
 
 // Starts the server to begin listening
